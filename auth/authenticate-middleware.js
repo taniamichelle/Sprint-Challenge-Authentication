ac3a3cfdedc9da06ef44ddef246b4ad84471f801
tania <tanislavens@yahoo.com>
2019-09-20 11:34:06 -0400

    installed dependencies. added users-model to auth directory. token auth for register, login, and jokes endpoints working

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,23 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken');
+
+const secrets = require('../config/secrets');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) { //bring in secret from secrets file below
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) { //token expired or is invalid
+        res.status(401).json({ error: 'You shall not pass!' });
+      } else { //token is good
+        req.user = { username: decodedToken.username };     //can add the username to req object. optional
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({ error: "No credentials provided." });
+  }
 };
