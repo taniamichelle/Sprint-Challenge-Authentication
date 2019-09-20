add5107de75a6cb2d50e418709f22358eac87457
tania <tanislavens@yahoo.com>
2019-09-20 16:18:07 -0400

    short an auth test

--- jokes/jokes.spec.js
+++ jokes/jokes.spec.js
@@ -1,23 +1,21 @@
 const db = require('../database/dbConfig');
 const request = require('supertest');
-const Jokes = require('../jokes/jokes-router');
+
+const server = require('../api/server');
+const Users = require('../auth/users-model');
 
 describe('Jokes', () => {
     describe('GET /', function () {
-        it('responds with 200', function (done) {
-            return request(Jokes)
-                .get('/')
+        it('returns with 200', () => {
+            return request(server).get('/')
+                .expect(200)
                 .then(res => {
                     expect(res.status).toBe(200);
                 });
         });
     });
-    // describe('GET /jokes', function () {
-    //     it('responds with 200', function (done) {
-    //         return request(Jokes)
-    //             .get('/jokes')
-    //             .auth('username', 'password')
-    //             .set('Accept', 'application/json')
-    //     });
-    // });
+    it('should set environment to be testing', () => {
+        expect(process.env.DB_ENV).toBe('testing');
+    });
+
 });
